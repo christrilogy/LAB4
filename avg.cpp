3471c41443b2066bac1f35fcc768f27356363d8c
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-10 15:38:35 -0500

    Create avg.cpp

---
+++ avg.cpp
@@ -0,0 +1,22 @@
+// This program asks for three numbers, then
+//display the average of the numbers.
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+	double num1, num2, num, avg;
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
+	cout<< "Enter the third number: ";
+	cin >> num3;
+	avg = num1 + num2 + num3 /3;
+	cout << "the average is " << avg << endl;
+	return o;
+	
+	}
+	
