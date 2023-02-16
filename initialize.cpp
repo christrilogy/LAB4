6de50c907353dd2f825528daf85429f4f3057065
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-16 05:47:10 -0500

    Create initialize.cpp

---
+++ initialize.cpp
@@ -0,0 +1,36 @@
+// This program tests whether or not an initialized value 
+// is equal to a value input by the user 
+// Christian Morgan
+
+#include <iostream> 
+using namespace std; 
+
+
+int main( ) 
+
+{ 
+
+    int num1, num2;          
+
+
+    cout << "Please enter an integer" << endl; 
+    cin >> num1; 
+
+    cout << "Please enter an integer" << endl; 
+    cin >> num2; 
+
+
+    cout << "num1 = " << num1 << " and num2 = " << num2 << endl; 
+
+    if  (num1 == num2) {
+      cout << "The values are the same." << endl; 
+      cout << "Hey, that’s a coincidence!"<< endl; 
+  
+      }
+    else
+        cout << "The values are not the same" << endl; 
+
+
+  return 0;
+
+}
